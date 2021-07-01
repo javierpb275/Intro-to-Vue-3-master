6dc0be45780e3785d83580b34fd5f68fa806f80c
javierpb275 <javierpb275@gmail.com>
2021-07-01 19:44:18 +0200

    atribute binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A warm fuzzy pair of socks.' 
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
         }
     }
 })
