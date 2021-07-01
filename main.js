38a7d9be181020764926f7e050b75954a977eeaf
javierpb275 <javierpb275@gmail.com>
2021-07-01 20:06:39 +0200

    event handling

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: true,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      sizes: ["S", "M", "L", "XL"],
-      variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
-      ],
-    };
-  },
-});
+    data() {
+        return {
+            cart: 0,
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
