9ca1455a85ac9d3b85463f499c444a3f16c16174
javierpb275 <javierpb275@gmail.com>
2021-07-01 20:00:14 +0200

    list rendering

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            onSale: true
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["S", "M", "L", "XL"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+    };
+  },
+});
