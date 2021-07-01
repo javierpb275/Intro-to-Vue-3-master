38b9680850191d01d9bfc18e4b612146b3426b38
javierpb275 <javierpb275@gmail.com>
2021-07-01 20:39:38 +0200

    communicating events

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -19,10 +19,9 @@ app.component('product-display', {
           <p v-else>Out of Stock</p>
   
           <p>Shipping: {{ shipping }}</p>
-          
-
-          <product-details :details="details"></product-details>
-
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
   
           <div 
             v-for="(variant, index) in variants" 
@@ -39,6 +38,15 @@ app.component('product-display', {
             v-on:click="addToCart">
             Add to Cart
           </button>
+  
+          <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          @click="removeFromCart">
+          Remove Item
+        </button>
+
         </div>
       </div>
     </div>`,
@@ -56,7 +64,10 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+        },
+        removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
         },
         updateVariant(index) {
             this.selectedVariant = index
