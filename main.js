fa7eb197d4fab2ab991820942693830cf7c31fab
javierpb275 <javierpb275@gmail.com>
2021-07-01 19:51:15 +0200

    conditional rendering

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 100,
+            onSale: true
         }
     }
 })
